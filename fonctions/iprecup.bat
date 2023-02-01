07aaf57e00b15b6aa3da6a87f2a6f2e17a85fb81
Colibri <116650154+ColibriXD@users.noreply.github.com>
2023-02-01 19:44:54 +0100

    modification de carractère

--- fonctions/iprecup.bat
+++ fonctions/iprecup.bat
@@ -1,12 +1,12 @@
 @echo off
 
-rem Récupération de l'adresse IP de l'ordinateur
+rem Rï¿½cupï¿½ration de l'adresse IP de l'ordinateur
 for /f "tokens=2 delims=:" %%a in ('ipconfig ^| find "IPv4"') do set "IP=%%a"
 
 rem Enregistrement de l'adresse IP dans un fichier texte
 echo Adresse IP : %IP% > infos_ip.txt
 
 echo.
-echo Les informations IP ont été enregistrées dans le fichier infos_ip.txt
-echo %DATE% une ip à été recupere >> log.log
+echo Les informations IP ont ete enregistrees dans le fichier infos_ip.txt
+echo %DATE% une ip a ete recupere >> log.log
 pause
--- fonctions/spamip.bat
+++ fonctions/spamip.bat
@@ -6,7 +6,7 @@ echo        ip spam
 echo=========================
 
 set /p cible=Entrez l'adresse IP de l'ordinateur cible :
-set /p nb_ping=Entrez le nombre de ping à effectuer :
+set /p nb_ping=Entrez le nombre de ping a effectuer :
 for /l %%i in (1,1,%nb_ping%) do (
   ping %cible% 
 )
