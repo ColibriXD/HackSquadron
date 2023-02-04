44d7042baa4cd1d2658fd9a1986ca38ef5a969de
Colibri <116650154+ColibriXD@users.noreply.github.com>
2023-02-04 14:45:26 +0100

    debug 4

--- paramêtres/debug.bat
+++ paramêtres/debug.bat
@@ -6,16 +6,25 @@ echo   debug
 echo ===============
 echo.
 echo. 
+:choi
 set /p choice=entrez commande de debug :
 
 if "%choice%" == "/help" (
-echo liste des commandes :
-echo.
-echo 1. testfonction
-echo 2. addextension
-echo 3. deletextension
-echo.
-) else (
+ echo liste des commandes :
+ echo.
+ echo 1. testfonction
+ echo 2. addextension
+ echo 3. deletextension
+ echo.
+ goto :choi
+)else if "%choice%"==""testfonction" (
+ echo testfonction
+ goto :choi )
+
+ else if "%choice%"==""testfonction"(
+ echo testfonction
+ goto :choi 
+ else (
 echo Commande non reconnue. Utilisez "/help" pour avoir la liste des commandes.
 )
 pause
