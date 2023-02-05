22bf1b5df32e693aab66be6e4e56989bab9bb3f6
Colibri <116650154+ColibriXD@users.noreply.github.com>
2023-02-05 10:26:53 +0100

    paramêtres maj

--- paramêtres/parametres.bat
+++ paramêtres/parametres.bat
@@ -1,5 +1,34 @@
 echo off
 color a
 title parametres
-echo cc
-set /p 
+echo
+                                           __                           
+______  _____   _______ _____     _____  _/  |_ _______   ____    ______
+\____ \ \__  \  \_  __ \\__  \   /     \ \   __\\_  __ \_/ __ \  /  ___/
+|  |_> > / __ \_ |  | \/ / __ \_|  Y Y  \ |  |   |  | \/\  ___/  \___ \ 
+|   __/ (____  / |__|   (____  /|__|_|  / |__|   |__|    \___  >/____  >
+|__|         \/              \/       \/                     \/      \/ 
+
+echo.
+set /p choice=parametres :
+
+if "%choice%" == "/help" (
+ echo liste des commandes :
+ echo.
+ echo 1. testfonction
+ echo 2. addextension
+ echo 3. deletextension
+ echo.
+ goto :choi
+)else if "%choice%"==""testfonction" (
+ echo testfonction
+ goto :choi )
+
+ else if "%choice%"==""testfonction"(
+ echo testfonction
+ goto :choi 
+ else (
+echo Commande non reconnue. Utilisez "/help" pour avoir la liste des commandes.
+)
+pause
+ 
