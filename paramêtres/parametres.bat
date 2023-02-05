a2b8934d4a2924f0b6b28654b3a80e38f781bb0a
Colibri <116650154+ColibriXD@users.noreply.github.com>
2023-02-05 10:37:27 +0100

    paramêtres modification

--- paramêtres/parametres.bat
+++ paramêtres/parametres.bat
@@ -1,33 +1,26 @@
-echo off
-color a
+@echo off
+color 1F
 title parametres
-echo
-                                           __                           
-______  _____   _______ _____     _____  _/  |_ _______   ____    ______
-\____ \ \__  \  \_  __ \\__  \   /     \ \   __\\_  __ \_/ __ \  /  ___/
-|  |_> > / __ \_ |  | \/ / __ \_|  Y Y  \ |  |   |  | \/\  ___/  \___ \ 
-|   __/ (____  / |__|   (____  /|__|_|  / |__|   |__|    \___  >/____  >
-|__|         \/              \/       \/                     \/      \/ 
-
+echo =====================
+echo     parametres
+echo =====================
 echo.
 set /p choice=parametres :
 
 if "%choice%" == "/help" (
- echo liste des commandes :
- echo.
- echo 1. testfonction
- echo 2. addextension
- echo 3. deletextension
- echo.
- goto :choi
-)else if "%choice%"==""testfonction" (
- echo testfonction
- goto :choi )
-
- else if "%choice%"==""testfonction"(
- echo testfonction
- goto :choi 
- else (
+echo liste des commandes :
+echo.
+echo 1. testfonction
+echo 2. addextension
+echo 3. deletextension
+echo.
+) else if "%choice%" == "/delete" (
+echo testfonction
+) else if "%choice%" == "/bug" (
+echo addextension
+) else if "%choice%" == "/guithub" (
+echo https://github.com/ColibriXD/modmenu
+) else (
 echo Commande non reconnue. Utilisez "/help" pour avoir la liste des commandes.
 )
 pause
